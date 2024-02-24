ec43f523bf84cec2b3cdaca72e02fafa58dea768
Joao Marcos <joao.marcos.witcel@gmail.com>
2024-02-23 23:27:16 -0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,8 @@ int main(int argc, char **argv)
 {
   printf("Olá mundo da emulação Chip-8!\n");
 
+  // Agora seria interessante fazer um esboço inicial de como vou separar
+  // a emulação do código que faz a exibição do output, pra deixar mais abstraído da plataforma.
+
   return 0;
 }
