0a45e3fab5913349e30aa142a69835e2841e1842
Joao Marcos <joao.marcos.witcel@gmail.com>
2024-02-28 21:35:07 -0300

    modelando CPU

--- TODO.md
+++ TODO.md
@@ -1,5 +1,10 @@
 # Ideias
 
+# Onde parei na leitura
+
+subtítulo: '8-bit Delay Timer' da página: [https://austinmorlan.com/posts/chip8_emulator/](https://austinmorlan.com/posts/chip8_emulator/)
+
+
 # Pendências
 
 * configurar SDL2 e abrir uma janela -- OK
--- src/chip8_machine.cc
+++ src/chip8_machine.cc
@@ -1,7 +1,22 @@
 #pragma once
 
+#include <stdint.h>
+
 typedef struct Chip8_Machine {
-  // Memória
-  // Registadores
+  // Memória de 4096 bytes, range 0x000-0xFFF
+  uint8_t memory[4096];
+  // 16 registradores de 8 bits, sendo que o 16º é usado para armazenar o resultados de operaçẽos
+  uint8_t registers[16];
+  // Registrador de índice
+  uint16_t index_register;
+  // Contador PC
+  uint16_t program_counter;
+  // stack de 16 níveis
+  uint16_t stack[16];
+  // stack pointer (stack índice)
+  uint8_t stack_pointer;
+
+  // Memória de vídeo, estou usando cores 'rgba', um byte por canal, apesar de ser monocromático na prática
+  uint32_t screen_buffer[64 * 32];
   // outras coisas
 } Chip8_Machine;
