b6e85b6c1357cde6566f81d084f503974676402f
Joao Marcos <joao.marcos.witcel@gmail.com>
2024-03-11 22:18:07 -0300

    teste e doc

--- src/utils.macro.h
+++ src/utils.macro.h
@@ -1,6 +1,10 @@
 #ifndef _UTILS_MACRO_H_
 #define _UTILS_MACRO_H_
 
+/**
+ * @brief declara um expressão vazia para suprimir eventuais avisos
+ * de falta de uso.
+ */
 #define UNUSED(EXPR) {(void)(EXPR);}
 
 #endif // _UTILS_MACRO_H_
--- tests/main.cc
+++ tests/main.cc
@@ -4,6 +4,8 @@
 
 #include "../src/chip8_machine.cc"
 
+#include "../src/utils.macro.h"
+
 void test_op_00E0(void)
 {
   Chip8_Machine machine = {};
@@ -11,6 +13,31 @@ void test_op_00E0(void)
   machine.memory[0] = 32;
   execute_op_00E0(&machine, 0x00E0);
   assert(machine.memory[0] == 0);
+}
+
+void test_op_3xkk(void)
+{
+  Chip8_Machine machine = {};
+  
+  {
+    uint16_t opcode = 0x33AE;
+
+    machine.registers[3] = 0xAE;
+    machine.program_counter = 0;
+
+    execute_op_3xkk(&machine, opcode);
+    assert(machine.program_counter == 2);
+  }
+
+  {
+    uint16_t opcode = 0x33AE;
+
+    machine.registers[3] = 0x00;
+    machine.program_counter = 0;
+
+    execute_op_3xkk(&machine, opcode);
+    assert(machine.program_counter == 0);
+  }
 }
 
 /**
@@ -22,7 +49,10 @@ void test_op_00E0(void)
  */
 int main(void)
 {
+  UNUSED(load_rom);
+
   test_op_00E0();
+  test_op_3xkk();
 
   return EXIT_SUCCESS;
 }
