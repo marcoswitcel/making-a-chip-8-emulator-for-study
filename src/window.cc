09189e68f39a77b49f138e00a3f0a20455587f2a
Joao Marcos <joao.marcos.witcel@gmail.com>
2024-04-27 16:31:23 -0300

    wip: sistema de áudio

--- src/window.cc
+++ src/window.cc
@@ -30,6 +30,15 @@ typedef struct Context_Data {
   bool fullscreen;
 } Context_Data;
 
+constexpr int AMPLITUDE = 28000;
+constexpr int FREQUENCY = 44100;
+
+typedef struct Audio_Beeper {
+  bool is_beeping;
+  double frequency;
+  double v; // @note Não entendi o que o 'v' representa
+} Audio_Beeper;
+
 /**
  * @brief 
  * 
@@ -350,9 +359,49 @@ static void render_scene(SDL_Renderer *renderer, Chip8_Machine *chip8_machine, C
   SDL_RenderPresent(renderer);
 }
 
-void beeper_audio_callback(void * a, uint8_t *b, int c)
+void generate_beep_samples_on_demand(Audio_Beeper *beeper , int16_t *audio_stream, const int length)
 {
-  // @todo João, implementar aqui um código que cheque se uma certa flag está setada e nesse caso emita o som
+  if (!beeper->is_beeping)
+  {
+    // memset aqui, acho que SDL tem um menset cross plataform
+    for (int i = 0; i < length; i++)
+    {
+      audio_stream[i] = 0;
+    }
+
+    return;
+  }
+
+  for (int i = 0; i < length; i++)
+  {
+    constexpr float volume_scale = 0.12;
+    audio_stream[i] = AMPLITUDE * std::sin(beeper->v * 2 * M_PI / FREQUENCY) * volume_scale;
+    beeper->v += beeper->frequency;
+  }
+}
+
+/**
+ * @brief Função callback passado ao audio device exposto pelo SDL
+ * 
+ * @param user_data contém a referência para a instância beeper que foi usada para configurar
+ * dispositivo de audio
+ * @param _audio_stream 
+ * @param _length 
+ */
+void beeper_audio_callback(void * user_data, uint8_t *_audio_stream, int _length)
+{
+  assert(user_data != NULL);
+  assert(_audio_stream != NULL);
+
+  Audio_Beeper *beeper = (Audio_Beeper *) user_data;
+  // pelo que entendi o formato real dependendo do parâmetro passado ao atributo format do `SDL_AudioSpec`
+  int16_t *audio_stream = (int16_t *) _audio_stream;
+
+  assert(_length % 2 == 0);
+  int length = _length / 2;
+
+  // @todo João, revisar se é necessário alguma forma de lock
+  generate_beep_samples_on_demand(beeper, audio_stream, length);
 }
 
 static void handle_events_and_inputs(SDL_Window *window, SDL_Renderer *renderer, Context_Data *context, bool *should_quit, Chip8_Machine *chip8_machine)
@@ -612,21 +661,23 @@ int open_window(const char *filename)
     return EXIT_FAILURE;
   }
 
-  constexpr int AMPLITUDE = 28000;
-  constexpr int FREQUENCY = 44100;
 
   SDL_AudioSpec intended_spec;
+
+  
+  Audio_Beeper audio_beeper = { .is_beeping = false, .frequency = 550, .v = 0, };
 
   intended_spec.freq = FREQUENCY;
   intended_spec.format = AUDIO_S16SYS;
   intended_spec.channels = 1;
   intended_spec.samples = 2048;
   intended_spec.format = AUDIO_S16SYS;
-  intended_spec.userdata = NULL; // passar aqui os dados relevantes
+  intended_spec.userdata = &audio_beeper;
   intended_spec.callback = beeper_audio_callback;
 
   SDL_AudioSpec real_spec;
-  SDL_OpenAudio(&intended_spec, &real_spec);
+  assert(SDL_OpenAudio(&intended_spec, &real_spec) == 0 );
+  assert(real_spec.format == AUDIO_S16SYS);
 
   SDL_PauseAudio(0);
   
@@ -704,6 +755,9 @@ int open_window(const char *filename)
       // - Uma ideia seria usar um método que atualiza o input para o frame ou a nível de ciclo
       // - Criar um método de faz o submit do 'evento' de 'press down' e 'released'?
       execute_a_cycle(chip8_machine);
+
+      // atualizando beeper
+      audio_beeper.is_beeping = chip8_machine.sound_timer > 0;
     }
 
     // Renderiza
